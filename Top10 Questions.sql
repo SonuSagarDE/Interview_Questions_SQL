1bfc0edcbe6b39dd7031da268be91caaf7a68a8c
Sonu Sagar <122198404+SonuSagarDE@users.noreply.github.com>
2023-05-03 10:45:51 +0530

    Add files via upload

--- Top10 Questions.sql
+++ Top10 Questions.sql
@@ -1,3 +1,6 @@
-## Q1 How to find duplicates based 
+## Q1 How to find duplicates based on key
 Select emp_id,count(1) from employee group by emp_id
-having count(1)>1
+having count(1)>1;
+## Q2 How to delete duplicates 
+with cte as (Select *, row_number() over (partition by emp_id order by emp_id) as rn from employee)
+delete from cte where rn>1;
