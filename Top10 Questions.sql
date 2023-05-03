aab490e6116c19d4ae8ca3f4a88cf1de969ecde0
Sonu Sagar <122198404+SonuSagarDE@users.noreply.github.com>
2023-05-03 10:56:26 +0530

    Add files via upload

--- Top10 Questions.sql
+++ Top10 Questions.sql
@@ -4,3 +4,7 @@ having count(1)>1;
 ## Q2 How to delete duplicates 
 with cte as (Select *, row_number() over (partition by emp_id order by emp_id) as rn from employee)
 delete from cte where rn>1;
+## Q3 Difference between union and union all
+-- Union of two table will give unique but union all will give all the duplicates records as welll. 
+## Q4 Differnce between rank,row_number and dense_rank()
+
