8e43d00a53f5650b0f371d31839a2194d3afcb11
Sonu Sagar <122198404+SonuSagarDE@users.noreply.github.com>
2023-05-14 16:13:19 +0530

    Add files via upload

---
+++ Find Leaf,Inner and root of a tree.sql
@@ -0,0 +1,57 @@
+/*
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| p_id        | int  |
++-------------+------+
+id is the primary key column for this table.
+Each row of this table contains information about the id of a node and the id of its parent node in a tree.
+The given structure is always a valid tree.
+
+Each node in the tree can be one of three types:
+
+"Leaf": if the node is a leaf node.
+"Root": if the node is the root of the tree.
+"Inner": If the node is neither a leaf node nor a root node.
+Write an SQL query to report the type of each node in the tree.
+Input: 
+Tree table:
++----+------+
+| id | p_id |
++----+------+
+| 1  | null |
+| 2  | 1    |
+| 3  | 1    |
+| 4  | 2    |
+| 5  | 2    |
++----+------+
+Output: 
++----+-------+
+| id | type  |
++----+-------+
+| 1  | Root  |
+| 2  | Inner |
+| 3  | Leaf  |
+| 4  | Leaf  |
+| 5  | Leaf  |
++----+-------+
+*/
+
+Create table tree
+(
+id int,
+p_id int
+);
+
+insert into tree values(1,null);
+insert into tree values(2,1);
+insert into tree values(3,1);
+insert into tree values(4,2);
+insert into tree values(5,2);
+
+
+
+
+
+
