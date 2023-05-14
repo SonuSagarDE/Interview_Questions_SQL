9341663434813d6691961ac60afe07bb87447345
Sonu Sagar <122198404+SonuSagarDE@users.noreply.github.com>
2023-05-14 15:20:59 +0530

    Add files via upload

---
+++ Delete Duplicate Records.sql
@@ -0,0 +1,16 @@
+drop table if exists cars;
+create table if not exists cars
+(
+    id      int,
+    model   varchar(50),
+    brand   varchar(40),
+    color   varchar(30),
+    make    int
+);
+insert into cars values (1, 'Model S', 'Tesla', 'Blue', 2018);
+insert into cars values (2, 'EQS', 'Mercedes-Benz', 'Black', 2022);
+insert into cars values (3, 'iX', 'BMW', 'Red', 2022);
+insert into cars values (4, 'Ioniq 5', 'Hyundai', 'White', 2021);
+insert into cars values (5, 'Model S', 'Tesla', 'Silver', 2018);
+insert into cars values (6, 'Ioniq 5', 'Hyundai', 'Green', 2021);
+
