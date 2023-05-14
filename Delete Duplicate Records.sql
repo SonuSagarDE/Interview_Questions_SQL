2575431fa33aab5e0fda6dc300fc8de8f405a5c6
Sonu Sagar <122198404+SonuSagarDE@users.noreply.github.com>
2023-05-14 15:27:13 +0530

    Add files via upload

--- Delete Duplicate Records.sql
+++ Delete Duplicate Records.sql
@@ -14,3 +14,15 @@ insert into cars values (4, 'Ioniq 5', 'Hyundai', 'White', 2021);
 insert into cars values (5, 'Model S', 'Tesla', 'Silver', 2018);
 insert into cars values (6, 'Ioniq 5', 'Hyundai', 'Green', 2021);
 
+--  Data duplicated based on SOME of the columns--
+-- Requirement: Delete duplicate data from cars table. Duplicate record is identified based on the model and brand name--
+
+## Method 1 using unique indentifier----
+delete from cars
+where id in (
+Select max(id) as id
+from cars
+group by model,brand
+having count(1)>1)
+
+## Using Self join
